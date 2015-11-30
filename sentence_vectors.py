8efc5562452c8e36a228f42b62df393bda0dcd17
jderiu <jderiu@student.ethz.ch>
2015-11-30 18:06:08 +0100

    a

--- sentence_vectors.py
+++ sentence_vectors.py
@@ -177,12 +177,12 @@ def main():
 
     dev_set_iterator = sgd_trainer.MiniBatchIteratorConstantBatchSize(
         numpy_rng,
-        dev_set,
+        [dev_set],
         batch_size=batch_size,
         randomize=True)
 
     def predict_prob_batch(batch_iterator):
-        preds = numpy.hstack([pred_prob_fn(batch_x_q) for batch_x_q in batch_iterator])
+        preds = numpy.hstack([pred_prob_fn(batch_x_q[0]) for batch_x_q in batch_iterator])
         return preds[:batch_iterator.n_samples]
 
 
